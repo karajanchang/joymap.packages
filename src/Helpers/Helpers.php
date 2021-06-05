cc25d734efadaf92952677ed48046fee521f11e3
david <karajanchang@gmail.com>
2021-06-05 14:34:15 +0800

    [移除config_path()] 改放到 zhyu/packages 裡

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -1,18 +1,4 @@
 <?php
-
-if (!function_exists('config_path')) {
-    /**
-     * Get the configuration path.
-     *
-     * @param string $path
-     * @return string
-     */
-    function config_path($path = '')
-    {
-        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
-    }
-}
-
 if (!function_exists('floor_dec')) {
     function floor_dec($value, int $decimal = 2)
     {
