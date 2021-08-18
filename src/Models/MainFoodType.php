224c9a42613857b4b2cc779ebb288bf4fa3d61f2
Jason <jason@twdd.com.tw>
2021-08-18 17:32:22 +0800

    create MainFoodType Model

---
+++ src/Models/MainFoodType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MainFoodType extends Model
+{
+    protected $table = 'main_food_types';
+
+    protected $guarded = [];
+}
