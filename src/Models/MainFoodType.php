9c357e127f3e3b8d141a64927fed8eed50735765
dennytwdd <83942419+dennyTwdd@users.noreply.github.com>
2021-09-27 14:50:06 +0800

    Update MainFoodType.php

--- src/Models/MainFoodType.php
+++ src/Models/MainFoodType.php
@@ -8,4 +8,9 @@ class MainFoodType extends Model
     protected $table = 'main_food_types';
 
     protected $guarded = [];
+
+    public function foodTypes()
+    {
+        return $this->hasMany(FoodType::class, 'main_food_type_id', 'id');
+    }
 }
