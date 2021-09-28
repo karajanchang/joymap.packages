aa328734cf79ca6067d1c62c1327de4a87bf60cb
Jason <jason@twdd.com.tw>
2021-09-28 17:13:02 +0800

    create creditCardLog and PayLog Models

---
+++ src/Models/CreditCardLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Joymap\Models;
+
+
+class CreditCardLog extends Model
+{
+    protected $table = 'creditcard_logs';
+
+    protected $guarded = [];
+}
---
+++ src/Models/PayLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Joymap\Models;
+
+
+class PayLog extends Model
+{
+    protected $table = 'pay_logs';
+
+    protected $guarded = [];
+}
