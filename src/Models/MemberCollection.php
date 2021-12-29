9fa22ee959c9758d2d8ec0c4239036d7d7da5ea4
Jason <jason@twdd.com.tw>
2021-12-29 12:14:45 +0800

    create MemberCollection Model

---
+++ src/Models/MemberCollection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class MemberCollection extends Model
+{
+    use HasFactory;
+
+    protected $table = 'member_collection';
+
+    protected $guarded = [];
+    
+}
