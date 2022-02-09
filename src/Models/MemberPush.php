47c3242a375315f1f159f5c07c3f39573920d83e
Jason <jason@twdd.com.tw>
2022-02-09 11:18:06 +0800

    create member push model

---
+++ src/Models/MemberPush.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class MemberPush extends Model
+{
+    use HasFactory;
+
+    protected $table = 'member_push';
+
+    protected $guarded = [];
+}
