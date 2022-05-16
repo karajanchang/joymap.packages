9d973177f982facb8fb8610e8a11adb366b377f6
Jason <jason@twdd.com.tw>
2022-05-16 16:48:45 +0800

    create FirmCooperation Models

---
+++ src/Models/FirmCooperation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class FirmCooperation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'firm_cooperation';
+
+    protected $guarded = [];
+
+}
