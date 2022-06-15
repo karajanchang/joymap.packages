6af6acf0a9aaeae3e504410de3fc2446770d5ce6
Jason <jason@twdd.com.tw>
2022-06-15 15:22:54 +0800

    create Bank and MemberBank models

---
+++ src/Models/MemberBank.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class MemberBank extends Model
+{
+    use HasFactory;
+
+    protected $table = "member_bank";
+
+    protected $guarded  = [];
+
+}
