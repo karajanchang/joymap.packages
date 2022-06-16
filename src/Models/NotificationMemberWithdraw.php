4b6e38ecd8fdb065f5af2a9eb23a9ee39c350ccc
Jason <jason@twdd.com.tw>
2022-06-16 14:14:38 +0800

    create notification member withdraw model

---
+++ src/Models/NotificationMemberWithdraw.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class NotificationMemberWithdraw extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notification_member_withdraw';
+
+    protected $guarded = [];
+
+    public function getMorphClass()
+    {
+        return $this->getTable();
+    }
+
+    public function notify()
+    {
+        return $this->morphOne(Notification::class, 'notify');
+    }
+}
