353bf639e8349235383c9e67c58f76ae85a89f91
Jason <jason@twdd.com.tw>
2022-08-17 11:53:46 +0800

    create jc coin model

---
+++ src/Models/JcCoin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class JcCoin extends Model
+{
+    use HasFactory;
+
+    protected $table = "jc_coins";
+
+    protected $guarded  = [];
+}
