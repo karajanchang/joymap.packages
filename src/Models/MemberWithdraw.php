02f97344c29c132ac0c1f3c00fa62d1b4c9f978a
Jason <jason@twdd.com.tw>
2022-09-15 16:25:00 +0800

    update relation

--- src/Models/MemberWithdraw.php
+++ src/Models/MemberWithdraw.php
@@ -19,7 +19,7 @@ class MemberWithdraw extends Model
 
     public function bank()
     {
-        return $this->belongsTo(Bank::class, 'id', 'bank_id');
+        return $this->belongsTo(Bank::class);
     }
 
 }
