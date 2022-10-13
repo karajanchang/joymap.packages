6d8beee4be4dce2888e551effa30b658953642ba
Jason <jason@twdd.com.tw>
2022-10-13 14:22:28 +0800

    create store action logs model

---
+++ src/Models/StoreActionLog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StoreActionLog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'store_action_logs';
+
+    protected $guarded = [];
+}
