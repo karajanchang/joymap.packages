333666fcb01be7a8ba888d2142979052672fad3f
willy <zxcxz20103@twdd.com.tw>
2023-03-28 17:07:40 +0800

    Create Wallet.php StoreWithdraw.php

---
+++ src/Models/StoreWithdraw.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Joymap\Models;
+
+
+class StoreWithdraw extends Model
+{
+    protected $table = 'store_withdraw';
+
+    protected $guarded = [];
+}
---
+++ src/Models/Wallet.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wallet';
+
+    protected $guarded = [];
+
+}
