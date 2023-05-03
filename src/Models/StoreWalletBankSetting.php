affee93e22a14eea9584ac124bb7ba0b8da51ed2
Musih <musih@twdd.com.tw>
2023-05-03 18:25:51 +0800

    儲值金Table[門市電子錢包]

---
+++ src/Models/StoreWalletBankSetting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StoreWalletBankSetting extends Model
+{
+    use HasFactory;
+
+    protected $table = 'store_wallet_bank_settings';
+
+    protected $guarded = [];
+
+    public function storeWallet()
+    {
+        return $this->belongsTo(StoreWallet::class);
+    }
+
+    public function bank()
+    {
+        return $this->belongsTo(bank::class);
+    }
+}
---
+++ src/Models/StoreWalletWithdraw.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StoreWalletWithdraw extends Model
+{
+    use HasFactory;
+
+    protected $table = 'store_wallet_withdraws';
+
+    protected $guarded = [];
+
+    public function storeWalletTransactionRecord()
+    {
+        return $this->belongsTo(StoreWalletTransactionRecord::class);
+    }
+
+    public function adminUser()
+    {
+        return $this->belongsTo(AdminUser::class);
+    }
+}
