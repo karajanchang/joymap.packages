0630a5c39639264e92eb53eac307e23bd9f5deef
kenLinTWDD <a861252012@gmail.com>
2024-03-05 12:20:14 +0800

    新增宜睿票劵交易紀錄model

---
+++ src/Models/EdenredTicketTransaction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joymap\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class EdenredTicketTransaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'edenred_ticket_transactions';
+
+    protected $guarded = [];
+
+}
